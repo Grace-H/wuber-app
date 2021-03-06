14ed3ec643c965498e8a415bcf81d23376c47ec2
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2021-03-06 13:54:25 -0600

    Working toggle-able date picker

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Input } from 'react-native-elements';
+import React, {useState} from 'react';
+import { StyleSheet, Text, ScrollView } from 'react-native';
+import { Input, Button, Icon, colors } from 'react-native-elements';
 import RNDateTimePicker from '@react-native-community/datetimepicker';
 
 export default function App() {
+  const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState(new Date());
+  const [showDate, setShowDate] = useState(false);
+  const [showTime, setShowTIme] = useState(false);
+
+  const onDateChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+
+  const onTimeChange = (event, selectedTime) => {
+    const currentTime = selectedDate || time;
+    setTime(currentTime);
+  };
+
+  const showDatePicker = () => {
+    setShowDate(!showDate);
+  };
+
+  const showTimePicker = () => {
+    setShowTime(!true);
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Input
         placeholder='Search' 
         label="Departing from"
@@ -16,10 +39,28 @@ export default function App() {
         label="Going to"
         rightIcon={{ type: 'font-awesome', name: 'search' }}
       />
+      <Button
+        title={"Date " + (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear() }
+        type="clear"
+        iconRight
+        titleStyle={styles.label}
+        icon={{ type: 'font-awesome', name: 'chevron-down' }}
+        buttonStyle={{justifyContent: "space-between"}}
+        onPress={showDatePicker}
+      />
+      {showDate && <RNDateTimePicker value={date} mode="date" style={{ width: "100%" }} onChange={onDateChange} />}
+      <Button
+          title={"Time " + time}
+          type="clear"
+          iconRight
+          titleStyle={styles.label}
+          icon={{ type: 'font-awesome', name: 'chevron-down' }}
+          buttonStyle={{justifyContent: "space-between"}}
+          onPress={showTimePicker}
+        />
+      {showTime && <RNDateTimePicker value={time} mode="time" style={{ width: "100%" }} />}
 
-    <RNDateTimePicker value={new Date()} mode="date" style={{ width: "100%" }} />
-
-    </View>
+    </ScrollView>
   );
 }
 
@@ -27,8 +68,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  label: {
+    color: colors.grey3, 
+    fontWeight: "bold",
+    fontSize: 16,
+  }
 });
 
