e536d52a800f557921958278fa80d506ca759781
User <User@MacBook-Pro-2.local>
2021-03-06 19:53:43 -0600

    Basic Navigation Code added

--- App.js
+++ App.js
@@ -3,10 +3,20 @@ import { StyleSheet, Text, ScrollView, View } from 'react-native';
 import { Input, Button, Header, colors } from 'react-native-elements';
 import RNDateTimePicker from '@react-native-community/datetimepicker';
 import { render } from 'react-dom';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+
+const Stack = createStackNavigator(); 
 
 export default function App() {
-    return(<View><Text>Hello</Text></View>);
-}
+  return( 
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "" component = {LoginScreen} />
+        <Stack.Screen name = "Home" component = {HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );}
 
 const styles = StyleSheet.create({
   container: {
