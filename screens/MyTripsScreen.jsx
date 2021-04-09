e8bfee42c954cbb888cdbad1fb8860f7f0e7c0b1
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2021-04-08 19:39:17 -0500

    Date formatting on mytrips screen

--- screens/MyTripsScreen.jsx
+++ screens/MyTripsScreen.jsx
@@ -25,13 +25,31 @@ import axios from 'axios';
  * },]
  */
 class MyTripsScreen extends Component {
-
+    
     constructor(props){
         super(props);
         this.state = {
             trips: [],
             userid: "606cd4960520b9ce1ac31c5b",
         }
+    }
+
+    formatTime(t){
+
+        const daysOftheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const monthsOftheYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+        const time = new Date(t);
+
+
+        return (
+            daysOftheWeek[time.getDay()] + ', ' + 
+            monthsOftheYear[time.getMonth() + 1] + ' ' + 
+            time.getDate() + ', ' + 
+            (time.getHours() - 12 <= 0 ? time.getHours() : time.getHours() - 12) + ':' + 
+            (time.getMinutes() < 10 ? '' + time.getMinutes() : '') + time.getMinutes() + " " + 
+            (time.getHours() - 12 <= 0 ? "AM" : "PM")
+        );
     }
 
     getTrips() {
@@ -44,8 +62,6 @@ class MyTripsScreen extends Component {
                 }
             })
             .catch(err => console.log(err));
-        console.log(this.state.trips);
-        console.log(this.state.user);
         return this.state.trips;
     }
 
@@ -60,7 +76,7 @@ class MyTripsScreen extends Component {
                             >
                             <RegisteredTripCard
                                 isDriver={item.driver == this.state.userid}
-                                date={item.time}
+                                date={this.formatTime(item.time)}
                                 destination={item.destination}
                                 departure={item.origin}
                             />
