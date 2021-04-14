f137bde93f8b9b4150928c5a3b1bcfcdfdf5a285
Gordon <gordon.olson@my.wheaton.edu>
2021-04-14 16:38:53 -0500

    Added some classes for use in Login

---
+++ screens/login/index.js
@@ -0,0 +1,30 @@
+import { isSignedIn } from 'auth.js';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signedIn: false,
+      checkedSignIn: false
+    };
+  }
+
+  componentDidMount() {
+    isSignedIn()
+      .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+      .catch(err => alert("An error occurred"));
+  }
+
+  render() {
+    const { checkedSignIn, signedIn } = this.state;
+
+    // If we haven't checked AsyncStorage yet, don't render anything (better ways to do this)
+    if (!checkedSignIn) {
+      return null;
+    }
+
+    const Layout = createRootNavigator(signedIn);
+    return <Layout />;
+  }
+}
