443f7684cb6ea2ee04fb06a24c18e2fe92c9878a
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2021-04-20 16:06:24 -0500

    searchUser route for users collection

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,21 +1,35 @@
-const router = require('express').Router();
-let User = require('../models/user.model');
+const router = require("express").Router();
+let User = require("../models/user.model");
 
-router.route('/').get((req, res) => {
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error: ' + err));
+//get all users
+router.route("/").get((req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
+//find one user's profile using their username and password
+router.route("/searchUser").get((req, res) => {
+  User.findOne()
+    .where("username")
+    .equals(req.query.username)
+    .where("password")
+    .equals(req.query.password)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
-    const newUser = new User({name, email});
+//add a user
+router.route("/add").post((req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
 
-    newUser.save()
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+  const newUser = new User({ name, email });
+
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
