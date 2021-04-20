821bae28a7d6e1bafe2ea5620573afbf2259f46c
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2021-04-20 16:20:30 -0500

    update route for users collection

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -32,4 +32,26 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+//update user
+//put id in place of :id in route (e.g. "users/update/3298hf98fdifbvsdijfbvsdf9")
+//pass user object as second param as would for add
+router.route("/update/:id").post((req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      //update all fields
+      user.name = req.body.name;
+      user.email = req.body.email;
+      user.password = req.body.password;
+      user.year = req.body.year;
+      user.phone = req.body.phone;
+      user.gender = req.body.gender;
+
+      user
+        .save()
+        .then(() => res.json("User updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
 module.exports = router;
